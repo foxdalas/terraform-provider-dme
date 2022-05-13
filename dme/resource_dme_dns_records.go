d244b59a3de4792a0a73e02df7a30e3bd73c62f0
Maxim Pogozhiy <foxdalas@gmail.com>
2022-05-13 15:05:23 +0700

    Fix update record

--- dme/resource_dme_dns_records.go
+++ dme/resource_dme_dns_records.go
@@ -235,7 +235,14 @@ func resourceManagedDNSRecordActionsCreate(d *schema.ResourceData, m interface{}
 	if err != nil {
 		if strings.Contains(err.Error(), "already exists") && d.Get("allow_overwrite").(bool) {
 			log.Printf("[DEBUG] DNS Record already exists however we are overwriting it")
-			cont, err = dmeClient.Update(&recordAttr, "dns/managed/"+d.Get("domain_id").(string)+"/records/")
+
+			record, err := dmeClient.GetbyId("dns/managed/" + d.Get("domain_id").(string) + "/records?recordName=" + recordAttr.Name + "&type=" + recordAttr.Type)
+			if err != nil {
+				return err
+			}
+			log.Println("Found record: ", record)
+
+			cont, err = dmeClient.Update(&recordAttr, "dns/managed/"+d.Get("domain_id").(string)+"/records/"+fmt.Sprintf("%v", record.S("id")))
 			if err != nil {
 				log.Println("Error returned: ", err)
 				return err
